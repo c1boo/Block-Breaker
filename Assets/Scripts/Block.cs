1d0bf21135f57578728b4d8ed3637d0b46b4e0f4
c1boo <mehmetali.cibo@gmail.com>
2023-02-25 16:25:30 +0100

    Added block breaking sound

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Block : MonoBehaviour
-{
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
+public class Block : MonoBehaviour {
+    //Config params
+    [SerializeField] AudioClip breakSound;
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
